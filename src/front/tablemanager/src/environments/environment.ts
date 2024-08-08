e672296e9c69e0dea861be5d62f87411ad79b5fe
Renar Riitsaar <renar.riitsaar@gmail.com>
2024-08-08 16:06:21 +0300

    PDF backend

--- src/front/tablemanager/src/environments/environment.ts
+++ src/front/tablemanager/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-  stripe: '',
+  stripe: 'pk_test_51Pl7V7RoGaLilpUFphwzkwitBGQnEXtwPScgHiA6U0y5Tmpbm2vxRxjGqHuXFdOHJeqhvuR25AZptNyMXBEwA6Yb00Kd8FbCZ8',
   serverUrl: 'http://localhost:8080',
 };
